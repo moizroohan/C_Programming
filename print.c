9fa0bba4cdc7ba5d3e3ee8938c8c6204752c1010
Moiz Roohan <76082378+moizroohan@users.noreply.github.com>
2023-09-07 23:25:35 +0500

    C Print

---
+++ print.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+void main(void){
+
+float a=50.5;
+float b=100.5;
+int c=50;
+int d=100;
+char e='8';
+char f[]="moiz moham";
+
+printf("------------------------------------------------------");
+printf("\n");
+printf("*******************My First Program*******************");
+printf("\n");
+printf("------------------------------------------------------");
+printf("\n");
+printf("\t-My First no is : %d",c);
+printf("\n");
+printf("\t-My second no is : %d ",d);
+printf("\n");
+printf("\t-The Sum Of No 1 & No 2 is = %d",c+d);
+printf("\n");
+printf("------------------------------------------------------");
+printf("\n");
+printf("\t-My First Floating no is : %f",a);
+printf("\n");
+printf("\t-My second Floating no is : %f ",b);
+printf("\n");
+printf("\t-The Sum Of Floating No 1 & Floating No 2 is = %f",a+b);
+printf("\n");
+printf("------------------------------------------------------");
+printf("\n");
+printf("\t-My First Character is : %c",e);
+printf("\n");
+printf("------------------------------------------------------");
+printf("\n");
+printf("\t-My First String is : %s",f);
+printf("\n");
+printf("------------------------------------------------------");
+printf("\n");
+}
